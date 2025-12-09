08a23f313589746178cbea966a7581816f245700
karman07 <84911866+karman07@users.noreply.github.com>
2025-12-09 13:16:35 +0530

    done

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -114,7 +114,7 @@ export default function Login() {
               <p className="text-sm text-gray-600 dark:text-gray-400">
                 Don't have an account?{" "}
                 <a 
-                  href="http://147.93.27.245:8005/" 
+                  href="https://srsregister.dine3d.com/" 
                   target="_blank" 
                   rel="noopener noreferrer"
                   className="text-green-700 font-semibold hover:underline hover:text-green-900 transition-colors duration-300"
--- src/base/Base.js
+++ src/base/Base.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "http://147.93.27.245:8001"
+export const BASE_URL = "https://srsbackend.dine3d.com"
